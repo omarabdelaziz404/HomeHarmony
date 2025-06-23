3b8f91a8daf1832e294dc9d0d5c15821859444fd
{omarabdelaziz404} <nourmotrash2003@gmail.com>
2025-06-23 14:33:40 +0300

    Production 5

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,19 +1,15 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
+import { neonConfig, Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 import ws from "ws";
 
-// Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 neonConfig.webSocketConstructor = ws;
 const connectionString = `${process.env.DATABASE_URL}`;
 
-// Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
+// Create a Neon Pool and pass it to PrismaNeon
 const pool = new Pool({ connectionString });
-
-// Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
 const adapter = new PrismaNeon(pool);
 
-// Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
 export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
     product: {
@@ -27,7 +23,6 @@ export const prisma = new PrismaClient({ adapter }).$extends({
           return Product.rating.toString();
         },
       },
-   
     },
     cart: {
       itemsPrice: {
@@ -94,4 +89,4 @@ export const prisma = new PrismaClient({ adapter }).$extends({
 
 
 
- 
+
